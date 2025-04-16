fa677aa9693732bb670163dac772cdc314ed42b0
Manthan Patel <tech.manthanpatel@gmail.com>
2025-04-17 02:22:07 +0530

    Delete AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/Variables.py

--- AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/Variables.py
+++
@@ -1,9 +0,0 @@
-Name = "Manthan Patel"
-Age = 20
-Mail = "tech.manthanpatel@gmail.com"
-CGPA = 8.59
-
-print("Myself" , Name)
-print("Age Of Mine is" , Age)
-print("Mail Address" , Mail)
-print("CGPA" , CGPA)
