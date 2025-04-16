70eb6033a17966a8c6951d5fa558bea41b238da5
Manthan Patel <tech.manthanpatel@gmail.com>
2025-04-17 02:21:53 +0530

    Delete AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/InputsInPython.py

--- AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/InputsInPython.py
+++
@@ -1,7 +0,0 @@
-Name = input("Enter Your Name")
-EnrollmentNo = int(input("Enter Your EnrollmentNo"))
-Marks = float(input("Enter Your Marks"))
-
-print("Your Name" ,Name)
-print("Your EnrollmentNo" ,EnrollmentNo)
-print("Your Marks" ,Marks)
