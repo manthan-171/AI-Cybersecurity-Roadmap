e76a04954afb0f665f62373b92c2af0c1e2673b1
Manthan Patel <tech.manthanpatel@gmail.com>
2025-04-17 01:33:11 +0530

    Add files via upload

---
+++ AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/TypesOfOperators.py
@@ -0,0 +1,58 @@
+#Arithmetic Operators
+
+A = 20
+B = 5
+
+print("Sum",A+B)
+print("Diff",A-B)
+print("Divide",A/B)
+print("Multiply",A*B)
+print("Reminder",A%B)
+
+#Relational Operator
+
+a = 10
+b = 20
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#Assignment Operator
+
+Num = 17
+
+#Num = Num+5
+Num += 5
+print(Num)
+Num -= 5
+print(Num)
+Num *= 5
+print(Num)
+Num /= 5
+print(Num)
+Num %= 5
+print(Num)
+Num **= 5
+print(Num)
+
+#Logical Operator
+
+X = 10
+Y = 20
+print(not True)
+print(not (X > Y))
+
+
+Value1 = True
+Value2 = True
+print("And Operator" , Value1 and Value2) #If Both Values True => True Else False 
+Value1 = True
+Value2 = False
+print("Or Operator" , Value1 or Value2)#If Any Value is True => True
+
+print("Using Expression With And" , (Value1 > Value2) and (Value1 == Value2))
+print("Using Expression With Or " , (Value1 > Value2) or (Value1 == Value2))
