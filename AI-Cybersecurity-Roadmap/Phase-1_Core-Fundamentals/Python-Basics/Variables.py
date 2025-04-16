277d78ff299ad657063daffaf33b1f51ee033421
Manthan Patel <tech.manthanpatel@gmail.com>
2025-04-16 22:50:43 +0530

    Update and rename variables.py to Variables.py

---
+++ AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/Variables.py
@@ -0,0 +1,9 @@
+Name = "Manthan Patel"
+Age = 20
+Mail = "tech.manthanpatel@gmail.com"
+CGPA = 8.59
+
+print("Myself" , Name)
+print("Age Of Mine is" , Age)
+print("Mail Address" , Mail)
+print("CGPA" , CGPA)
