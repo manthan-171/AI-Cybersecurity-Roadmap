8bcb6c4a64acfb3ef9ca4df08851ee7e80286e08
Manthan Patel <tech.manthanpatel@gmail.com>
2025-04-17 01:45:37 +0530

    Add files via upload

---
+++ AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/TypeConversion&Casting.py
@@ -0,0 +1,17 @@
+#Type Conversion
+
+A = 10
+B = 20.5
+
+Sum = A + B # 10.0 + 20.5 Because Float Is Bigger So It Will Convert The Value Of A To Float 
+print("Sum" , Sum)
+
+#Type Casting 
+
+A = int("2")
+B = 10
+
+Sum = A + B
+
+print("Sum" , Sum)
+print(type(A))
