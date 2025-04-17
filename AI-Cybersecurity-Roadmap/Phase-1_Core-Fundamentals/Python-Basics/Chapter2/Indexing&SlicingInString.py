36a50f9c3203f3c7f83c29dbf1086a337449bcfe
Manthan Patel <tech.manthanpatel@gmail.com>
2025-04-18 00:25:26 +0530

    Add files via upload

---
+++ AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/Chapter2/Indexing&SlicingInString.py
@@ -0,0 +1,13 @@
+#Indexing
+
+str = "Dream Big. Start small. Act now"
+print(str[0],str[1],str[2],str[3],str[4],str[5])
+print(str[28],str[29],str[30])
+
+#Slicing
+
+str1 = "Less Talk More Action"
+print(str1[0:4])
+print(str1[11:])
+print(str1[:18])
+print(str1[-6:])
---
+++ AI-Cybersecurity-Roadmap/Phase-1_Core-Fundamentals/Python-Basics/Chapter2/StringFunction.py
@@ -0,0 +1,28 @@
+#String Function
+
+#Concatenation
+str1 = "Less Talk"
+str2 = "more Action"
+
+print(str1," "+str2)
+
+#Length Of A String
+print(len(str1))
+print(len(str2))
+
+#Ends With?
+print(str1.endswith("lk"))
+
+#capitalizes
+print(str2.capitalize())
+
+#Replace
+print(str1.replace("L","M"))
+
+#Find
+print(str2.find("o"))
+
+#Count
+str3 = "Success needs effort and effort shapes Success"
+print(str3.count("Success"))
+print(str3.count("c"))
